95357cf44781c34377e93c169cdcdc5ad606690a
Muhammad hashim <mh4165209@gmail.com>
2026-08-15 10:54:50 +0500

    react project create to vite and all file arrugnment complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { StrictMode, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+ 
+  // <App/>
+}
+
+export default App
